0bcb32f16b7eedf850309dda749dc51b3276596e
Renato Massao Yonamine <renato.massao@fbiz.com.br>
2015-06-19 16:01:15 -0300

    Atualizado binding de evento e removido um atributo

--- assets/plugins/microshare.js
+++ assets/plugins/microshare.js
@@ -1,15 +1,10 @@
-$('[data-microshare]').on('click',function(){
+$(document).on('[data-microshare]',function(e){
+	e.preventDefault();
 
-	var shareService = $(this).attr('data-microshare-service').toLowerCase();
-	var shareScheme;
-	var shareUrl;
-	var shareTitle = $(this).attr('data-microshare-title');
-
-	if ( $(this).attr('data-microshare-url') ) {
-		shareUrl = $(this).attr('data-microshare-url');
-	} else {
-		shareUrl = document.URL;
-	}
+	var shareScheme,
+		shareService = $(this).attr('data-microshare').toLowerCase(),
+		shareTitle = $(this).attr('data-microshare-title') || document.title,
+		shareUrl = $(this).attr('data-microshare-url') || document.URL;
 
 	if ( shareService === 'facebook' || shareService === 'fb' ) {
 		shareScheme = 'https://www.facebook.com/sharer/sharer.php?u=';
@@ -18,11 +13,7 @@ $('[data-microshare]').on('click',function(){
 	} else if ( shareService === 'googleplus' || shareService === 'gplus' ) {
 		shareScheme = 'https://plus.google.com/share?url=';
 	} else if ( shareService === 'in' || shareService === 'linkedin' ) {
-		if ( shareTitle === '' ) {
-			shareScheme = 'https://www.linkedin.com/shareArticle?mini=true&url='+shareUrl+'&title='+ document.title +'&summary=&source=';
-		} else {
-			shareScheme = 'https://www.linkedin.com/shareArticle?mini=true&url='+shareUrl+'&title='+shareTitle+'&summary=&source=';
-		}
+		shareScheme = 'https://www.linkedin.com/shareArticle?mini=true&url='+shareUrl+'&title='+shareTitle+'&summary=&source=';
 	}
 
 	window.open(shareScheme+shareUrl,'_blank','height=400, width=800');
