e37c2287e14c9f41eb16fcd7c1428f35e47b4789
Renato Massao Yonamine <rmy.yonamine@gmail.com>
2015-07-24 19:00:22 -0300

    Adicionado .gitignore, mudança de arquivo index para .html, alterado como as redes sociais são definidas no arquivo para facilitar adição de novas redes

--- assets/plugins/microshare.js
+++ assets/plugins/microshare.js
@@ -1,23 +1,33 @@
 $(function() {
-	$(document).on('[data-microshare]',function(e){
+	$(document).on('click', '[data-microshare]', function(e){
 		e.preventDefault();
 
 		var shareScheme,
 			shareService = $(this).attr('data-microshare').toLowerCase(),
 			shareTitle = $(this).attr('data-microshare-title') || document.title,
-			shareUrl = $(this).attr('data-microshare-url') || document.URL;
+			shareUrl = $(this).attr('data-microshare-url') || document.URL,
+			shareMap = [{
+					name: ['facebook', 'fb'],
+					link: 'https://www.facebook.com/sharer/sharer.php?u='
+				}, {
+					name: ['twitter', 'tw'],
+					link: 'https://twitter.com/home?status='
+				}, {
+					name: ['googleplus', 'gplus'],
+					link: 'https://plus.google.com/share?url='
+				}, {
+					name: ['linkedin', 'in'],
+					link: 'https://www.linkedin.com/shareArticle?mini=true&url='+shareUrl+'&title='+shareTitle+'&summary=&source='
+				}];
 
-		if ( shareService === 'facebook' || shareService === 'fb' ) {
-			shareScheme = 'https://www.facebook.com/sharer/sharer.php?u=';
-		} else if ( shareService === 'twitter') {
-			shareScheme = 'https://twitter.com/home?status=';
-		} else if ( shareService === 'googleplus' || shareService === 'gplus' ) {
-			shareScheme = 'https://plus.google.com/share?url=';
-		} else if ( shareService === 'in' || shareService === 'linkedin' ) {
-			shareScheme = 'https://www.linkedin.com/shareArticle?mini=true&url='+shareUrl+'&title='+shareTitle+'&summary=&source=';
-		}
+			for(var x in shareMap) {
+				if(shareMap[x].name.indexOf(shareService) > -1) {
+					shareScheme = shareMap[x].link + shareUrl;
+					break;
+				}
+			}
 
-		window.open(shareScheme+shareUrl,'_blank','height=400, width=800');
+		window.open(shareScheme,'_blank','height=400, width=800');
 
 	});
 });
